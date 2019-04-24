29bcda530bc7b30a5bc67cb4228246d92424fa5c
Akshya Gupta <agupta@snaproute.com>
2019-04-24 12:47:53 -0700

    Added steps to build go-control-plane binary

---
+++ front-proxy-tcp-eds-go-control-plane/go-control-plane-steps/main.go
@@ -0,0 +1,237 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+
+	"sync"
+	"sync/atomic"
+
+	"github.com/envoyproxy/go-control-plane/pkg/cache"
+	xds "github.com/envoyproxy/go-control-plane/pkg/server"
+
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+
+	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/endpoint"
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
+)
+
+var (
+	debug bool
+
+	localhost = "192.168.1.11"
+
+	port uint
+
+	mode string
+
+	version int32
+
+	config cache.SnapshotCache
+)
+
+const (
+	XdsCluster = "xds_cluster"
+	Ads        = "ads"
+	Xds        = "xds"
+	Rest       = "rest"
+)
+
+func init() {
+	flag.BoolVar(&debug, "debug", true, "Use debug logging")
+	flag.UintVar(&port, "port", 5678, "Management server port")
+	flag.StringVar(&mode, "xds", Xds, "Management server type (ads, xds, rest)")
+}
+
+type logger struct{}
+
+func (logger logger) Infof(format string, args ...interface{}) {
+	log.Infof(format, args...)
+}
+func (logger logger) Errorf(format string, args ...interface{}) {
+	log.Errorf(format, args...)
+}
+func (cb *callbacks) Report() {
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+	log.WithFields(log.Fields{"fetches": cb.fetches, "requests": cb.requests}).Info("cb.Report()  callbacks")
+}
+func (cb *callbacks) OnStreamOpen(ctx context.Context, id int64, typ string) error {
+	log.Infof("OnStreamOpen %v:%v open for %s", ctx, id, typ)
+	return nil
+}
+func (cb *callbacks) OnStreamClosed(id int64) {
+	log.Infof("OnStreamClosed %d closed", id)
+}
+func (cb *callbacks) OnStreamRequest(int64, *v2.DiscoveryRequest) error {
+	log.Infof("OnStreamRequest")
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+	cb.requests++
+	if cb.signal != nil {
+		close(cb.signal)
+		cb.signal = nil
+	}
+	return nil
+}
+func (cb *callbacks) OnStreamResponse(int64, *v2.DiscoveryRequest, *v2.DiscoveryResponse) {
+	log.Infof("OnStreamResponse...")
+	cb.Report()
+}
+func (cb *callbacks) OnFetchRequest(ctx context.Context, req *v2.DiscoveryRequest) error {
+	log.Infof("OnFetchRequest for ctx %v", ctx)
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+	cb.fetches++
+	if cb.signal != nil {
+		close(cb.signal)
+		cb.signal = nil
+	}
+	return nil
+}
+func (cb *callbacks) OnFetchResponse(*v2.DiscoveryRequest, *v2.DiscoveryResponse) {}
+
+type callbacks struct {
+	signal   chan struct{}
+	fetches  int
+	requests int
+	mu       sync.Mutex
+}
+
+// Hasher returns node ID as an ID
+type Hasher struct {
+}
+
+// ID function
+func (h Hasher) ID(node *core.Node) string {
+	if node == nil {
+		return "unknown"
+	}
+	return node.Id
+}
+
+const grpcMaxConcurrentStreams = 1000000
+
+// RunManagementServer starts an xDS server at the given port.
+func RunManagementServer(ctx context.Context, server xds.Server, port uint) {
+	var grpcOptions []grpc.ServerOption
+	grpcOptions = append(grpcOptions, grpc.MaxConcurrentStreams(grpcMaxConcurrentStreams))
+	grpcServer := grpc.NewServer(grpcOptions...)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.WithError(err).Fatal("failed to listen")
+	}
+
+	// register services
+	discovery.RegisterAggregatedDiscoveryServiceServer(grpcServer, server)
+	v2.RegisterEndpointDiscoveryServiceServer(grpcServer, server)
+	v2.RegisterClusterDiscoveryServiceServer(grpcServer, server)
+	v2.RegisterListenerDiscoveryServiceServer(grpcServer, server)
+
+	log.Infof("Will start the server")
+	log.WithFields(log.Fields{"port": port}).Info("management server listening")
+	go func() {
+		if err = grpcServer.Serve(lis); err != nil {
+			log.Error(err)
+		}
+	}()
+	<-ctx.Done()
+	log.WithFields(log.Fields{"port": port}).Info("management server listening started")
+
+	grpcServer.GracefulStop()
+}
+
+func main() {
+	flag.Parse()
+	if debug {
+		log.SetLevel(log.DebugLevel)
+	}
+	ctx := context.Background()
+
+	log.Printf("Starting control plane")
+
+	signal := make(chan struct{})
+	cb := &callbacks{
+		signal:   signal,
+		fetches:  0,
+		requests: 0,
+	}
+	config = cache.NewSnapshotCache(false, Hasher{}, logger{})
+
+	srv := xds.NewServer(config, cb)
+
+	// start the xDS server
+	go RunManagementServer(ctx, srv, port)
+
+	<-signal
+	cb.Report()
+
+	atomic.AddInt32(&version, 1)
+	nodeId := config.GetStatusKeys()
+	log.Infof("Getting the nodeId as %v", nodeId)
+
+	var service_name = "serviceendpoints"
+	log.Infof(">>>>>>>>>>>>>>>>>>> creating endpoints group " + service_name)
+
+	e := []cache.Resource{
+		&v2.ClusterLoadAssignment{
+			ClusterName: service_name,
+			Endpoints: []endpoint.LocalityLbEndpoints{
+				{
+					Locality: nil,
+					LbEndpoints: []endpoint.LbEndpoint{
+						{
+							HostIdentifier: &endpoint.LbEndpoint_Endpoint{
+								Endpoint: &endpoint.Endpoint{
+									Address: &core.Address{
+										Address: &core.Address_SocketAddress{
+											SocketAddress: &core.SocketAddress{
+												Protocol: core.TCP,
+												Address:  "172.20.0.3",
+												PortSpecifier: &core.SocketAddress_PortValue{
+													PortValue: 8200,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						{
+							HostIdentifier: &endpoint.LbEndpoint_Endpoint{
+								Endpoint: &endpoint.Endpoint{
+									Address: &core.Address{
+										Address: &core.Address_SocketAddress{
+											SocketAddress: &core.SocketAddress{
+												Protocol: core.TCP,
+												Address:  "172.20.0.4",
+												PortSpecifier: &core.SocketAddress_PortValue{
+													PortValue: 8200,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// =================================================================================
+
+	log.Infof(">>>>>>>>>>>>>>>>>>> creating snapshot Version " + fmt.Sprint(version))
+	snap := cache.NewSnapshot(fmt.Sprint(version), e, nil, nil, nil)
+
+	config.SetSnapshot("id_1", snap)
+	select {}
+
+}
